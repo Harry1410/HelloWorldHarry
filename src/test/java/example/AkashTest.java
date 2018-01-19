6eec4c85d752bd92266b125cc443bed136139d9c
Harry1410 <harpal_navy@yahoo.com>
2018-01-19 16:07:22 +0530

    first commit

---
+++ src/test/java/example/AkashTest.java
@@ -0,0 +1,22 @@
+package example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+ 
+
+public class AkashTest 
+	extends TestCase
+{
+      public void testSlow() {
+        System.out.println("Aakash slow");
+      }
+
+      public void testSlower() {
+        System.out.println("Aakash slower");
+      }
+
+      public void testFast() {
+        System.out.println("Aakash fast");
+      }
+    }
---
+++ src/test/java/example/HelloServletTest.java
@@ -0,0 +1,16 @@
+package example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+ 
+
+public class HelloServletTest 
+	extends TestCase
+{
+      public void testSlow() {
+        System.out.println("Test Hello Servlet");
+      }
+
+
+    }
